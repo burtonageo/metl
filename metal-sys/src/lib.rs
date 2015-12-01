5d4e8d7a719a2b8ac9b7fb7628e75292c7fb8e59
George Burton <burtonageo@gmail.com>
2015-12-01 20:38:24 +0000

    Initial module structure

--- metal-sys/src/lib.rs
+++ metal-sys/src/lib.rs
@@ -1,3 +1,13 @@
+extern crate cocoa;
+extern crate core_foundation;
+extern crate core_graphics;
+extern crate libc;
+extern crate objc;
+
+mod classes;
+mod functions;
+mod protocols;
+
 #[test]
 fn it_works() {
 }
