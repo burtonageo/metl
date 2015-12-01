90510320f541268a6a1db0f4876c1a4fc5881b8b
George Burton <burtonageo@gmail.com>
2015-12-01 23:45:15 +0000

    Basic facade

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
-#[test]
-fn it_works() {
+extern crate metal_sys;
+
+pub mod sys {
+    pub use metal_sys::*;
 }
+
+pub type ClearColor = sys::MTLClearColor;
+pub type Origin = sys::MTLOrigin;
+pub type Region = sys::MTLRegion;
+pub type ScissorRect = sys::MTLScissorRect;
+pub type Size = sys::MTLSize;
+pub type Viewport = sys::MTLViewport;
+pub type DrawPrimitivesIndirectArguments = sys::MTLDrawPrimitivesIndirectArguments;
+pub type DrawIndexedPrimitivesIndirectArguments = sys::MTLDrawIndexedPrimitivesIndirectArguments;
+pub type DispatchThreadgroupsIndirectArguments = sys::MTLDispatchThreadgroupsIndirectArguments;
