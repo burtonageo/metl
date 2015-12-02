e8c5fd115fe87cc953221303ebbb856a15635280
George Burton <burtonageo@gmail.com>
2015-12-02 07:35:12 +0000

    Fully create MTLDevice interface

--- metal-sys/src/protocols.rs
+++ metal-sys/src/protocols.rs
@@ -1,10 +1,14 @@
 use block::Block;
 use cocoa::base::{BOOL, id};
 use cocoa::foundation::{NSString, NSUInteger};
-// use libc::c_void;
-use {MTLFeatureSet, MTLSize};
+use libc::c_void;
+use {MTLFeatureSet, MTLPipelineOption, MTLSize};
 
 pub type MTLNewLibraryCompletionHandler = Block<(id,), id>;
+pub type MTLNewRenderPipleineStateCompletionHandler = Block<(id, id), ()>;
+pub type MTLNewRenderPipelineStateWithReflectionCompletionHandler = Block<(id, id, id), ()>;
+pub type MTLNewComputePipelineStateCompletionHandler = Block<(id, id), ()>;
+pub type MTLNewComputePipelineStateWithReflectionCompletionHandler = Block<(id, id, id), ()>;
 
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 pub trait MTLDevice {
@@ -17,14 +21,64 @@ pub trait MTLDevice {
     unsafe fn supportsTextureSampleCount(self, textureSampleCount: NSUInteger) -> BOOL;
 
     unsafe fn newDefaultLibrary(self) -> id;
-    unsafe fn newLibraryWithFile_Error(self, filePath: id, error: id) -> id;
-    unsafe fn newLibraryWithSource_Options_CompletionHandler(self,
+    unsafe fn newLibraryWithFile_error(self, filePath: id, error: id) -> id;
+    unsafe fn newLibraryWithSource_options_completionHandler(self,
                                                              source: id,
                                                              options: id,
                                                              completionHandler: MTLNewLibraryCompletionHandler);
-    unsafe fn newLibraryWithSource_Options_Error(self, source: id, options: id, error: id);
-    unsafe fn newLibraryWithData_Error(self, data: id, error: id);
+    unsafe fn newLibraryWithSource_options_error(self, source: id, options: id, error: id);
+    unsafe fn newLibraryWithData_error(self, data: id, error: id);
 
     unsafe fn newCommandQueue(self) -> id;
     unsafe fn newCommandQueueWithMaxCommandBufferCount(self, maxCommandBufferCount: NSUInteger) -> id;
+
+    unsafe fn newBufferWithLength_Options(self, length: NSUInteger, options: id) -> id;
+    unsafe fn newBufferWithBytes_Length_Options(self, pointer: *mut c_void, length: NSUInteger, options: id) -> id;
+    unsafe fn newBufferWithBytesNoCopy_length_options_deallocator(self,
+                                                                  pointer: *mut c_void,
+                                                                  length: NSUInteger,
+                                                                  deallocator: Block<(*mut c_void, NSUInteger), ()>);
+
+    unsafe fn newTextureWithDescriptor(self, descriptor: id) -> id;
+    unsafe fn newSamplerStateWithDescriptor(self, descriptor: id) -> id;
+
+    unsafe fn newStencilDepthStateWithDescriptor(self, descriptor: id) -> id;
+    unsafe fn newRenderPipleineStateWithDescriptor_completionHandler(
+                                                    self,
+                                                    completionHandler: MTLNewRenderPipleineStateCompletionHandler);
+    unsafe fn newRenderPipelineStateWithDescriptor_options_completionHandler(
+                                        self,
+                                        options: MTLPipelineOption,
+                                        completionHandler: MTLNewRenderPipelineStateWithReflectionCompletionHandler);
+    unsafe fn newRenderPipelineStateWithDescriptor_error(self, error: id);
+    unsafe fn newRenderPipelineStateWithDescriptor_options_reflection(self,
+                                                                      options: MTLPipelineOption,
+                                                                      reflection: id,
+                                                                      error: id);
+
+    unsafe fn newComputePipelineStateWithFunction_completionHandler(
+                                                        self,
+                                                        function: id,
+                                                        completionHandler: MTLNewComputePipelineStateCompletionHandler);
+    unsafe fn newComputePipelineStateWithFunction_options_completionHandler(
+                                        self,
+                                        function: id,
+                                        options: MTLPipelineOption,
+                                        completionHandler: MTLNewComputePipelineStateWithReflectionCompletionHandler);
+    unsafe fn newComputePipelineStateWithFunction_error(self, function: id, error: id);
+    unsafe fn newComputePipelineStateWithFunction_options_reflection_error(self,
+                                                                           function: id,
+                                                                           options: MTLPipelineOption,
+                                                                           reflection: id,
+                                                                           error: id);
+
+    unsafe fn newComputePipelineStateWithDescriptor_options_completionHandler(self,
+                                        descriptor: id,
+                                        options: MTLPipelineOption,
+                                        completionHandler: MTLNewComputePipelineStateWithReflectionCompletionHandler);
+    unsafe fn newComputePipelineStateWithDescriptor_options_reflection_error(self,
+                                                                             descriptor: id,
+                                                                             options: MTLPipelineOption,
+                                                                             reflection: id,
+                                                                             error: id);
 }
