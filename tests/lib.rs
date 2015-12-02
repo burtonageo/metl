ea20303f7bd855b9d543a4b863055a6aefdb51f6
George Burton <burtonageo@gmail.com>
2015-12-02 22:32:30 +0000

    Add CommandQueue label test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,7 +1,7 @@
 extern crate mtl;
 extern crate libc;
 
-use mtl::{Device};
+use mtl::{CommandQueue, Device};
 
 #[test]
 fn get_device() {
@@ -13,4 +13,15 @@ fn get_device() {
 fn get_device_name() {
     let device = Device::system_default_device().unwrap();
     assert!(!device.name().into_owned().is_empty());
+}
+
+#[test]
+fn set_get_command_queue_label() {
+    let device = Device::system_default_device().unwrap();
+    let command_queue = CommandQueue::new(&device).unwrap();
+
+    let dummy_command_queue_name = "foo";
+    command_queue.set_label(dummy_command_queue_name);
+
+    assert_eq!(command_queue.get_label().into_owned(), dummy_command_queue_name.to_string());
 }
