363aa9f9f02ef4b363476ababccadf1922925082
George Burton <burtonageo@gmail.com>
2015-12-02 21:32:52 +0000

    Re-export Block types

--- metal-sys/src/protocols/mod.rs
+++ metal-sys/src/protocols/mod.rs
@@ -1,3 +1,7 @@
 mod mtl_device;
 
 pub use self::mtl_device::MTLDevice;
+pub use self::mtl_device::{MTLNewComputePipelineStateCompletionHandler,
+                           MTLNewComputePipelineStateWithReflectionCompletionHandler, MTLNewLibraryCompletionHandler,
+                           MTLNewRenderPipelineStateWithReflectionCompletionHandler,
+                           MTLNewRenderPipleineStateCompletionHandler};
