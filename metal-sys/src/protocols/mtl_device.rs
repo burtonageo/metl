5bd0146974deaa5c67bc8a9fc32d4d7179befe47
George Burton <burtonageo@gmail.com>
2015-12-03 09:09:36 +0000

    Doc spree

--- metal-sys/src/protocols/mtl_device.rs
+++ metal-sys/src/protocols/mtl_device.rs
@@ -6,20 +6,102 @@ use cocoa::foundation::NSUInteger;
 use libc::c_void;
 use {MTLFeatureSet, MTLPipelineOption, MTLSize};
 
-pub type MTLNewLibraryCompletionHandler = Block<(id,), id>;
-pub type MTLNewRenderPipleineStateCompletionHandler = Block<(id, id), ()>;
-pub type MTLNewRenderPipelineStateWithReflectionCompletionHandler = Block<(id, id, id), ()>;
-pub type MTLNewComputePipelineStateCompletionHandler = Block<(id, id), ()>;
-pub type MTLNewComputePipelineStateWithReflectionCompletionHandler = Block<(id, id, id), ()>;
-
+/// The MTLDevice protocol defines the interface to a single graphics processor (GPU). You use
+/// an object that conforms to this protocol to query the capabilities of the processor and to
+/// allocate objects used to access those capabilities.
+///
+/// Your app does not define classes that implement this protocol; it is used by Metal to provide
+/// a device object to your app. To obtain a system device, call the MTLCreateSystemDefaultDevice
+/// function or select a result from the MTLCopyAllDevices function.
+///
+/// Most objects in Metal that perform graphics rendering and computational work are associated
+/// directly with a specific device. For example, texture objects are created by a device object
+/// and can be used only with that device. Most methods on a MTLDevice object create non-transient
+/// objects, including command queues, resources (such as buffers and textures), and pipeline states.
+/// These objects can be expensive to create and you are encouraged to create them soon after your
+/// app launches and reuse them throughout the lifetime of your app. Avoid creating these objects
+/// in performance sensitive code.
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 pub trait MTLDevice {
+    /// A Boolean value that indicates whether a device supports a packed depth/stencil buffer. (read-only)
+    ///
+    /// # Discussion
+    ///
+    /// If the value is YES, the device supports the MTLPixelFormatDepth24Unorm_Stencil8 pixel format.
     unsafe fn depth24Stencil8PixelFormatSupported(self) -> BOOL;
+
+    /// A Boolean value that indicates whether a device is configured as headless. (read-only)
+    ///
+    /// # Discussion
+    ///
+    /// If the value is YES, the device can not and does not have any displays attached.
     unsafe fn headless(self) -> BOOL;
+
+    /// Boolean value that indicates whether a device is low-power. (read-only)
+    ///
+    /// # Discussion
+    ///
+    /// On multi-GPU systems that support automatic graphics switching, this property will return YES
+    /// for the lower-power GPU.
     unsafe fn lowPower(self) -> BOOL;
+
+    /// The maximum number of threads along each dimension of a threadgroup. (read-only)
+    ///
+    /// # Discussion
+    ///
+    /// This is the maximum compute grid size (width, height, depth) supported by the device.
+    ///
+    /// For more information on the specific capabilities of each feature set, see [Metal Programming Guide](
+    /// https://developer.apple.com/library/prerelease/ios/documentation/
+    ///     Miscellaneous/Conceptual/MetalProgrammingGuide/Introduction/
+    ///     Introduction.html#//apple_ref/doc/uid/TP40014221
+    /// ).
     unsafe fn maxThreadsPerGroup(self) -> MTLSize;
+
+    /// A string that identifies the device. (read-only)
+    ///
+    /// # Discussion
+    ///
+    /// When this object is viewed in the Xcode debugger, its name is used to label the device.
+    /// The name should only be used for informational purposes.
     unsafe fn name(self) -> id;
+
+    /// Determines whether a device implements a particular feature set.
+    ///
+    /// # Parameters
+    ///
+    /// `featureSet` - A value that indicates the feature set you are interested in.
+    ///
+    /// # Return Value
+    ///
+    /// YES if the feature set is supported by the device; otherwise NO
+    ///
+    /// # Discussion
+    ///
+    /// For more information on the specific capabilities of each feature set, see [Metal Programming Guide](
+    /// https://developer.apple.com/library/prerelease/ios/documentation/
+    ///     Miscellaneous/Conceptual/MetalProgrammingGuide/Introduction/
+    ///     Introduction.html#//apple_ref/doc/uid/TP40014221
+    /// ).
     unsafe fn supportsFeatureSet(self, featureSet: MTLFeatureSet) -> BOOL;
+    
+    /// Determines whether a device supports a given texture sample count.
+    ///
+    /// # Parameters
+    ///
+    /// `textureSampleCount` - The sample count you are interested in.
+    ///
+    /// # Return Value
+    ///
+    /// YES if the sample count is supported by the device; otherwise NO.
+    ///
+    /// # Discussion
+    ///
+    /// For more information on the specific capabilities of each feature set, see [Metal Programming Guide](
+    /// https://developer.apple.com/library/prerelease/ios/documentation/
+    ///     Miscellaneous/Conceptual/MetalProgrammingGuide/Introduction/
+    ///     Introduction.html#//apple_ref/doc/uid/TP40014221
+    /// ).
     unsafe fn supportsTextureSampleCount(self, textureSampleCount: NSUInteger) -> BOOL;
 
     unsafe fn newDefaultLibrary(self) -> id;
@@ -257,3 +339,9 @@ impl MTLDevice for id {
                                                         error:error]
     }
 }
+
+pub type MTLNewLibraryCompletionHandler = Block<(id,), id>;
+pub type MTLNewRenderPipleineStateCompletionHandler = Block<(id, id), ()>;
+pub type MTLNewRenderPipelineStateWithReflectionCompletionHandler = Block<(id, id, id), ()>;
+pub type MTLNewComputePipelineStateCompletionHandler = Block<(id, id), ()>;
+pub type MTLNewComputePipelineStateWithReflectionCompletionHandler = Block<(id, id, id), ()>;
