07a467424c6e618b8cc9afe21754df1c442509df
George Burton <burtonageo@gmail.com>
2015-12-06 13:35:28 +0000

    Forgot to declare mod which `MTLDrawable` is defined in

--- metal-sys/src/protocols/mod.rs
+++ metal-sys/src/protocols/mod.rs
@@ -1,6 +1,7 @@
-mod mtl_device;
 mod mtl_command_buffer;
 mod mtl_command_queue;
+mod mtl_device;
+mod mtl_drawable;
 
 pub use self::mtl_command_buffer::MTLCommandBuffer;
 pub use self::mtl_command_queue::MTLCommandQueue;
