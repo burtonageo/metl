e2eb1cbab991bc732c2e4c94bfd862ef93cd7d6a
George Burton <georgeburton@georges-imac.home>
2015-12-06 08:58:16 +0000

    Print out the max threads per group in mtlinfo

--- examples/mtlinfo.rs
+++ examples/mtlinfo.rs
@@ -21,6 +21,10 @@ fn main() {
 
     println!("Device:\t\t\t\t\t\t{}", device.get_name());
     println!("Supports 24 bit stencil and 8 bit depth:\t{}", device.is_depth24_stencil8_pixel_format_supported());
+    {
+        let max_tpg = device.max_threads_per_group();
+        println!("Maximum threads per group:\t\t\t({}, {}, {})", max_tpg.width, max_tpg.height, max_tpg.depth);
+    }
     println!("Is low power:\t\t\t\t\t{}", device.is_low_power());
     println!("Is headless:\t\t\t\t\t{}", device.is_headless());
 }
