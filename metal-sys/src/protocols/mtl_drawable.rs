30ca35a132931d1f029205cd2b88b5cbcfdfbe61
George Burton <burtonageo@gmail.com>
2015-12-08 21:30:16 +0000

    Tabs => spaces

--- metal-sys/src/protocols/mtl_drawable.rs
+++ metal-sys/src/protocols/mtl_drawable.rs
@@ -4,39 +4,39 @@ use core_foundation::date::CFTimeInterval;
 /// The `MTLDrawable` protocol defines the interface for an object that represents
 /// a displayable resource that can be used as a destination for rendering commands.
 pub trait MTLDrawable {
-	/// Displays the displayable resource.
-	///
-	/// # Discussion
-	///
-	/// This method is usually invoked by the command buffer’s scheduled handler.
-	///
-	/// # Availability
-	///
-	/// Available in iOS 8.0 and later.
-	unsafe fn present(self);
+    /// Displays the displayable resource.
+    ///
+    /// # Discussion
+    ///
+    /// This method is usually invoked by the command buffer’s scheduled handler.
+    ///
+    /// # Availability
+    ///
+    /// Available in iOS 8.0 and later.
+    unsafe fn present(self);
 
-	/// Displays the displayable resource at a given host time.
-	///
-	/// # Parameters
-	///
-	/// `presentationTime` - Time to display the resource, in seconds.
-	///
-	/// # Discussion
-	///
-	/// This method is usually invoked by the command buffer’s scheduled handler.
-	/// 
-	/// # Availability
-	///
-	/// Available in iOS 8.0 and later.
-	unsafe fn presentAtTime(self, presentationTime: CFTimeInterval);
+    /// Displays the displayable resource at a given host time.
+    ///
+    /// # Parameters
+    ///
+    /// `presentationTime` - Time to display the resource, in seconds.
+    ///
+    /// # Discussion
+    ///
+    /// This method is usually invoked by the command buffer’s scheduled handler.
+    /// 
+    /// # Availability
+    ///
+    /// Available in iOS 8.0 and later.
+    unsafe fn presentAtTime(self, presentationTime: CFTimeInterval);
 }
 
 impl MTLDrawable for id {
-	unsafe fn present(self) {
-		msg_send![self, present]
-	} 
+    unsafe fn present(self) {
+        msg_send![self, present]
+    } 
 
-	unsafe fn presentAtTime(self, presentationTime: CFTimeInterval) {
-		msg_send![self, presentAtTime:presentationTime]
-	}
+    unsafe fn presentAtTime(self, presentationTime: CFTimeInterval) {
+        msg_send![self, presentAtTime:presentationTime]
+    }
 }
