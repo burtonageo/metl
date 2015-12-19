67d449eb7a9d47e514162140eb294062b58f3b61
George Burton <burtonageo@gmail.com>
2015-12-19 23:02:32 +0000

    Rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,8 @@ pub mod sys {
 
 pub use command_buffer::CommandBuffer;
 pub use command_queue::{CommandQueue, CommandQueueError};
-pub use compile_options::{CompileOptions, LanguageVersion, SpecificLanguageVersion, PreprocessorMacroValue};
+pub use compile_options::{CompileOptions, LanguageVersion, PreprocessorMacroValue,
+                          SpecificLanguageVersion};
 pub use device::{Device, DeviceError};
 pub use drawable::Drawable;
 pub use function::Function;
