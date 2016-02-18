d61b5ecc7bf895122a462cfc92d4b974aa63f6b1
George Burton <burtonageo@gmail.com>
2016-02-18 23:10:37 +0000

    rustfmt

--- metal-sys/src/classes/mtl_vertex_attribute.rs
+++ metal-sys/src/classes/mtl_vertex_attribute.rs
@@ -1,40 +1,40 @@
 use cocoa::base::id;
-use cocoa::foundation::NSUInteger
+use cocoa::foundation::NSUInteger;
 use objc::runtime::BOOL;
 use MTLDataType;
 
 pub trait MTLVertexAttribute {
-	/// The name of the attribute. (read-only)
-	unsafe fn name(self) -> id;
+    /// The name of the attribute. (read-only)
+    unsafe fn name(self) -> id;
 
-	/// The index of the attribute, as declared in Metal shader source code.
-	/// (read-only)
-	unsafe fn attributeIndex(self) -> id;
-	
-	/// The data type for the attribute, as declared in Metal shader source code.
-	/// (read-only)
-	unsafe fn attributeType(self) -> MTLDataType;
-	
-	/// A Boolean value that indicates whether this vertex attribute is active.
-	/// (read-only)
-	///
-	/// #Discussion
-	///
-	/// If NO, this attribute is inactive and can be ignored.
-	unsafe fn active(self) -> BOOL;
+    /// The index of the attribute, as declared in Metal shader source code.
+    /// (read-only)
+    unsafe fn attributeIndex(self) -> id;
+
+    /// The data type for the attribute, as declared in Metal shader source code.
+    /// (read-only)
+    unsafe fn attributeType(self) -> MTLDataType;
+
+    /// A Boolean value that indicates whether this vertex attribute is active.
+    /// (read-only)
+    ///
+    /// #Discussion
+    ///
+    /// If NO, this attribute is inactive and can be ignored.
+    unsafe fn active(self) -> BOOL;
 }
 
 impl MTLVertexAttribute for id {
-	unsafe fn name(self) -> id {
-		msg_send![self, name]
-	}
-	unsafe fn attributeIndex(self) -> id {
-		msg_send![self, attributeIndex]
-	}
-	unsafe fn attributeType(self) -> MTLDataType {
-		msg_send![self, attributeType]
-	}
-	unsafe fn active(self) -> BOOL {
-		msg_send![self, active]
-	}
+    unsafe fn name(self) -> id {
+        msg_send![self, name]
+    }
+    unsafe fn attributeIndex(self) -> id {
+        msg_send![self, attributeIndex]
+    }
+    unsafe fn attributeType(self) -> MTLDataType {
+        msg_send![self, attributeType]
+    }
+    unsafe fn active(self) -> BOOL {
+        msg_send![self, active]
+    }
 }
