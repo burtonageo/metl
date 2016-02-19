e1b65e11fc9507a33caf22abba717abda533785e
George Burton <burtonageo@gmail.com>
2016-02-19 07:33:34 +0000

    Add trait comment to MTLVertexAttribute, and fix return type of MTLVertexAttribute::attributeIndex

--- metal-sys/src/classes/mtl_vertex_attribute.rs
+++ metal-sys/src/classes/mtl_vertex_attribute.rs
@@ -3,13 +3,17 @@ use cocoa::foundation::NSUInteger;
 use objc::runtime::BOOL;
 use MTLDataType;
 
+/// A `MTLVertexAttribute` object represents an attribute for per-vertex input
+/// in a vertex shader function. You use vertex attribute objects to inspect
+/// the inputs of a shader function by examining the `vertexAttributes` property
+/// of the corresponding `MTLFunction` object.
 pub trait MTLVertexAttribute {
     /// The name of the attribute. (read-only)
     unsafe fn name(self) -> id;
 
     /// The index of the attribute, as declared in Metal shader source code.
     /// (read-only)
-    unsafe fn attributeIndex(self) -> id;
+    unsafe fn attributeIndex(self) -> NSUInteger;
 
     /// The data type for the attribute, as declared in Metal shader source code.
     /// (read-only)
@@ -28,7 +32,7 @@ impl MTLVertexAttribute for id {
     unsafe fn name(self) -> id {
         msg_send![self, name]
     }
-    unsafe fn attributeIndex(self) -> id {
+    unsafe fn attributeIndex(self) -> NSUInteger {
         msg_send![self, attributeIndex]
     }
     unsafe fn attributeType(self) -> MTLDataType {
