14997be19a8d2a113739ec71ef5b1fd4f7c4e281
George Burton <burtonageo@gmail.com>
2016-02-19 09:27:04 +0000

    Remove unused import

--- metal-sys/src/classes/mtl_argument.rs
+++ metal-sys/src/classes/mtl_argument.rs
@@ -1,6 +1,5 @@
 use cocoa::base::id;
 use cocoa::foundation::NSUInteger;
-use objc::runtime::BOOL;
 use MTLTextureType;
 
 /// A `MTLArgument` object describes a single argument to a Metal function. Your
