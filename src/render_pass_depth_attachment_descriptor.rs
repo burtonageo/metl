94c6c2100cc8980eb0d963c2d8c0c41b8b9bb237
George Burton <burtonageo@gmail.com>
2016-02-27 19:26:15 +0000

    Implement RenderPathDepthAttachmentDescriptor and RenderPassDescriptor methods which create it

---
+++ src/render_pass_depth_attachment_descriptor.rs
@@ -0,0 +1,42 @@
+use cocoa::base::id;
+use sys::{MTLMultisampleDepthResolveFilter, MTLRenderPassDepthAttachmentDescriptor};
+use FromRaw;
+
+pub struct RenderPathDepthAttachmentDescriptor(id);
+
+impl RenderPathDepthAttachmentDescriptor {
+    pub fn clear_depth(self) -> f64 {
+        unsafe { self.0.clearDepth() }
+    }
+
+    pub fn set_clear_depth(self, clear_depth: f64) {
+        unsafe { self.0.setClearDepth(clear_depth) }
+    }
+
+    pub fn depth_resolve_filter(self) -> MultisampleDepthResolveFilter {
+        unsafe { self.0.depthResolveFilter().into() }
+    }
+
+    pub fn set_depth_resolve_filter(self, resolve_filter: MultisampleDepthResolveFilter) {
+        unsafe { self.0.setDepthResolveFilter(resolve_filter.into()) }
+    }
+}
+
+impl Clone for RenderPathDepthAttachmentDescriptor {
+    fn clone(&self) -> Self {
+        unsafe { FromRaw::from_raw(self.0.copy()).unwrap() }
+    }
+}
+
+impl_from_into_raw!(RenderPathDepthAttachmentDescriptor,
+                    of class "MTLRenderPathDepthAttachmentDescriptor");
+
+convertible_enum! {
+    #[derive(Clone, Copy, Eq, Hash, PartialEq)]
+    pub enum MultisampleDepthResolveFilter: MTLMultisampleDepthResolveFilter {
+        Sample0 => MTLMultisampleDepthResolveFilterSample0,
+        Min => MTLMultisampleDepthResolveFilterMin,
+        Max => MTLMultisampleDepthResolveFilterMax
+    }
+
+}
--- src/render_pass_descriptor.rs
+++ src/render_pass_descriptor.rs
@@ -1,5 +1,56 @@
-use cocoa::base::id;
+use cocoa::base::{id, nil};
+use cocoa::foundation::NSUInteger;
+use sys::MTLRenderPassDescriptor;
+use {FromRaw, IntoRaw, RenderPathDepthAttachmentDescriptor};
 
 pub struct RenderPassDescriptor(id);
+
+#[allow(unused_variables)]
+impl RenderPassDescriptor {
+    pub fn new() -> Self {
+        unsafe { FromRaw::from_raw(MTLRenderPassDescriptor::renderPassDescriptor(nil)).unwrap() }
+    }
+
+    pub fn color_attachments(&self) -> ! {
+        unimplemented!();
+    }
+
+    pub fn set_color_attachments(&self, attachments: ()) {
+        unimplemented!();
+    }
+
+    pub fn depth_attachment(&self) -> RenderPathDepthAttachmentDescriptor {
+        unsafe { FromRaw::from_raw(self.0.depthAttachment()).unwrap() }
+    }
+
+    pub fn set_depth_attachment(&self, attachment: RenderPathDepthAttachmentDescriptor) {
+        unsafe { self.0.setDepthAttachment(attachment.into_raw()) }
+    }
+
+    pub fn stencil_attachment(&self) -> ! {
+        unimplemented!();
+    }
+
+    pub fn set_stencil_attachment(&self, attachment: ()) {
+        unimplemented!();
+    }
+
+    #[cfg(target_os = "macos")]
+    pub fn render_target_array_length(self) -> usize {
+        unsafe { self.0.renderTargetArrayLength() as usize }
+    }
+
+    #[cfg(target_os = "macos")]
+    pub fn set_render_target_array_length(self, target_array_length: usize) {
+        unsafe { self.0.setRenderTargetArrayLength(target_array_length as NSUInteger) }
+    }
+}
+
+impl Clone for RenderPassDescriptor {
+    fn clone(&self) -> Self {
+        let cloned = unsafe { self.0.copy() };
+        FromRaw::from_raw(cloned).unwrap()
+    }
+}
 
 impl_from_into_raw!(RenderPassDescriptor, of class "MTLRenderPassDescriptor");
