18dbd081283d055918e0a3062ec834025e2f1092
George Burton <burtonageo@gmail.com>
2016-02-27 18:57:03 +0000

    Add MTLRenderPassAttachmentDescriptor

---
+++ metal-sys/src/classes/mtl_render_pass_depth_attachment_descriptor.rs
@@ -0,0 +1,56 @@
+use cocoa::base::id;
+
+pub trait MTLRenderPassDepthAttachmentDescriptor {
+    /// The depth to use when the depth attachment is cleared.
+    ///
+    /// # Discussion
+    ///
+    /// The default value is 1.0.
+    ///
+    /// If the `loadAction` property of the attachment is set to
+    /// `MTLLoadActionClear`, then at the start of a rendering pass,
+    /// the contents of the texture are filled with the value stored
+    /// in the `clearDepth` property. Otherwise, `clearDepth` is ignored.
+    unsafe fn clearDepth(self) -> f64;
+    unsafe fn setClearDepth(self, clearDepth: f64);
+
+    /// The filter used for an MSAA depth resolve operation.
+    ///
+    /// # Discussion
+    ///
+    /// The default value is `MTLMultisampleDepthResolveFilterSample0`.
+    unsafe fn depthResolveFilter(self) -> MTLMultisampleDepthResolveFilter;
+    unsafe fn setDepthResolveFilter(self, resolveFilter: MTLMultisampleDepthResolveFilter);
+    
+    unsafe fn copy(self) -> self;
+}
+
+impl MTLRenderPassDepthAttachmentDescriptor for id {
+    unsafe fn clearDepth(self) -> f64 {
+        msg_send![self, clearDepth]
+    }
+
+    unsafe fn setClearDepth(self, clearDepth: f64) {
+        msg_send![self, setClearDepth:clearDepth]
+    }
+
+    unsafe fn depthResolveFilter(self) -> MTLMultisampleDepthResolveFilter {
+        msg_send![self, depthResolveFilter]
+    }
+
+    unsafe fn setDepthResolveFilter(self, resolveFilter: MTLMultisampleDepthResolveFilter) {
+        msg_send![self, setDepthResolveFilter:resolveFilter]
+    }
+
+    unsafe fn copy(self) -> self {
+        msg_send![self, copy]
+    }
+}
+
+#[repr(usize)]
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
+pub enum MTLMultisampleDepthResolveFilter {
+    MTLMultisampleDepthResolveFilterSample0 = 0,
+    MTLMultisampleDepthResolveFilterMin = 1,
+    MTLMultisampleDepthResolveFilterMax = 2,
+}
