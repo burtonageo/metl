949f37dde6b3d864b32607bc91f079d86b36880e
George Burton <burtonageo@gmail.com>
2016-04-06 16:45:17 +0100

    Fix event loop of metal_triangle example

--- examples/metal_triangle.rs
+++ examples/metal_triangle.rs
@@ -1,12 +1,22 @@
 extern crate metl;
 
 use metl::extras::window::WindowBuilder;
+use metl::extras::events::Event;
 
 fn main() {
     let window = WindowBuilder::new().build().unwrap();
     let _device = metl::Device::system_default_device().unwrap();
 
-    for event in window.poll_events() {
-        println!("{:?}", event);
+    'mainloop: loop {
+        for event in window.poll_events() {
+            match event {
+                Event::Closed => {
+                    break 'mainloop
+                }
+                _ => {}
+            }
+        }
+
+        // drawing code here
     }
 }
