05705f1f4575826d89287c8cb82a5fc6e4be39ee
George Burton <burtonageo@gmail.com>
2016-05-02 22:46:30 +0100

    Add stub for RenderPipelineDescriptor

--- src/render_pipeline_state.rs
+++ src/render_pipeline_state.rs
@@ -3,3 +3,7 @@ use cocoa::base::id;
 pub struct RenderPipelineState(id);
 
 impl_from_into_raw!(RenderPipelineState, of protocol "MTLRenderPipelineState");
+
+pub struct RenderPipelineDescriptor(id);
+
+impl_from_into_raw!(RenderPipelineState, of class "MTLRenderPipelineDescriptor");
