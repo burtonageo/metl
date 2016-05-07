2cbf17361ffb8415133b530184eb8607e22e3816
George Burton <burtonageo@gmail.com>
2016-05-07 17:03:32 +0100

    Add a delegate to the example

--- examples/metal_triangle.rs
+++ examples/metal_triangle.rs
@@ -1,9 +1,19 @@
 extern crate metl;
 
-use metl::extras::window::{Event, WindowBuilder};
+use metl::extras::window::{Event, WindowBuilder, View, ViewDelegate};
 use metl::{ClearColor, PixelFormat};
 
 fn main() {
+    struct MyViewDelegate;
+    impl ViewDelegate for MyViewDelegate {
+        fn draw_into_view(&self, view: &mut View) {
+            view.draw();
+        }
+        fn on_resize(&self, _view: &mut View, new_size: (f64, f64)) {
+            println!("Resized to {:?}", new_size);
+        }
+    }
+
     let device = metl::Device::system_default_device().unwrap();
     let mut window = WindowBuilder::new(device)
                      .with_dimensions(800, 600)
@@ -13,6 +23,7 @@ fn main() {
 
     window.view.set_depth_stencil_pixel_format(PixelFormat::Rgba8Unorm);
     window.view.set_clear_color(ClearColor::new(1.0, 0.0, 0.0, 1.0));
+    window.view.set_delegate(MyViewDelegate);
 
     'mainloop: loop {
         for event in window.poll_events() {
@@ -24,6 +35,5 @@ fn main() {
             }
         }
 
-        window.view.draw();
     }
 }
