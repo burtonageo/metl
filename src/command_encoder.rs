c21d691f931c63e09f332d801b9db6248b18022d
George Burton <burtonageo@gmail.com>
2016-10-25 23:12:05 +0100

    Add CommandEncoder methods

--- src/command_encoder.rs
+++ src/command_encoder.rs
@@ -1,5 +1,39 @@
-use cocoa::base::id;
+use cocoa::base::{id, nil};
+use cocoa::foundation::NSString;
+use std::ffi::CStr;
+use std::mem;
+use sys::MTLCommandEncoder;
+use Device;
 
+#[derive(Debug)]
 pub struct CommandEncoder(id);
 
-impl_from_into_raw!(CommandQueue, of protocol "MTLCommandEncoder");
+impl CommandEncoder {
+    pub fn end_encoding(&mut self) {
+        unsafe { self.0.endEncoding(); }
+    }
+
+    pub fn insert_debug_signpost<S: AsRef<str>>(&mut self, signpost: S) {
+        unsafe { self.0.insertDebugSignpost(NSString::alloc(nil).init_str(signpost.as_ref())) }
+    }
+
+    pub fn push_debug_group<S: AsRef<str>>(&mut self, debug_group: S) {
+        unsafe { self.0.pushDebugGroup(NSString::alloc(nil).init_str(debug_group.as_ref())) }
+    }
+
+    pub fn pop_debug_group(&mut self) {
+        unsafe { self.0.popDebugGroup(); }
+    }
+
+    pub fn device(&self) -> &Device {
+        unsafe { mem::transmute(&self.0.device()) }
+    }
+
+    pub fn label(&self) -> &str {
+        unsafe {
+            CStr::from_ptr(self.0.label().UTF8String()).to_str().unwrap_or(&"")
+        }
+    }
+}
+
+impl_from_into_raw!(CommandEncoder, of protocol "MTLCommandEncoder");
